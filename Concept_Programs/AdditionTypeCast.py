4d2168e04950674667ef70e718e6750e1f4a5920
Tejas Sutar <tejassutar0009@gmail.com>
2026-06-18 12:19:42 +0530

    Added new programs

---
+++ Concept_Programs/AdditionTypeCast.py
@@ -0,0 +1,9 @@
+print("Enter first Number : ")
+No1 = input()
+
+print("Enter second Number : ")
+No2 = input()
+
+Ans = int(No1) + int(No2)           #(int)No1 + (int)No2  => Error
+
+print("Addition is : ",Ans)
