99b92b0db4214d21c56f4be024e4ec45e09bcb0b
Tejas Sutar <tejassutar9001@gmail.com>
2026-07-01 11:32:21 +0530

    Added new programs

---
+++ Concept_Programs/FilterX.py
@@ -0,0 +1,20 @@
+def CheckEven(No):
+    return (No % 2 == 0)
+
+def main():
+    Data = [13,12,8,10,11,20]
+    
+    print("Input Data is : ",Data)
+    
+    FData = list(filter(CheckEven,Data))
+    
+    print("Data After Filter : ",FData)  
+    
+    
+    
+if __name__ == "__main__":
+    main()
+    
+    # Filter(Function, Iterable)
+    # Map(Function, Iterable)
+    # Reduce(Function, Iterable) => Single value(return) output
