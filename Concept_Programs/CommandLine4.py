e160b21023c1b7fdd5b79b8eef6656ac03064e7a
Tejas Sutar <tejassutar9001@gmail.com>
2026-07-25 07:44:01 +0530

    Added new programs

---
+++ Concept_Programs/CommandLine4.py
@@ -0,0 +1,8 @@
+import sys
+
+No1 = int(sys.argv[1])      # sys.argv[1] takes input in string
+No2 = int(sys.argv[2])
+
+Ans = No1 + No2
+
+print("Addition is : ",Ans)
---
+++ Concept_Programs/ProcessID.py
@@ -0,0 +1,7 @@
+import os
+
+print("PID of current process is : ",os.getpid())
+print("PID of parent process is : ",os.getppid())
+
+# ps => Linux, MacOS
+# tasklist => microsoft
